e4c1fce8d2542449b69de1b3a85df8701100f5ae
Robert Tulke <rt@debian.sh>
2026-04-11 01:18:09 +0200

    Add sshscan formula v3.5.0

---
+++ Formula/sshscan.rb
@@ -0,0 +1,37 @@
+class Sshscan < Formula
+  include Language::Python::Virtualenv
+
+  desc "SSH algorithm security scanner for auditing and compliance checking"
+  homepage "https://github.com/rtulke/sshscan"
+  url "https://github.com/rtulke/sshscan/archive/refs/tags/v3.5.0.tar.gz"
+  sha256 "REPLACE_WITH_RELEASE_SHA256"
+  license "MIT"
+
+  head "https://github.com/rtulke/sshscan.git", branch: "main"
+
+  depends_on "python@3.12"
+
+  resource "pyyaml" do
+    url "https://files.pythonhosted.org/packages/54/ed/79a089b6be93607fa5cdaedf301d7dfb23af5f25c398d5ead2525b063e17/PyYAML-6.0.2.tar.gz"
+    sha256 "REPLACE_WITH_PYYAML_SHA256"
+  end
+
+  def install
+    venv = virtualenv_create(libexec, "python@3.12")
+    venv.pip_install resource("pyyaml")
+
+    libexec.install "sshscan.py"
+
+    (bin/"sshscan").write <<~SHELL
+      #!/bin/bash
+      exec "#{libexec}/bin/python3" "#{libexec}/sshscan.py" "$@"
+    SHELL
+    chmod 0755, bin/"sshscan"
+  end
+
+  test do
+    assert_match "3.5.0", shell_output("#{bin}/sshscan --version")
+    assert_match "NIST", shell_output("#{bin}/sshscan --list-frameworks")
+    assert_match "weak", shell_output("#{bin}/sshscan --list-filter")
+  end
+end
